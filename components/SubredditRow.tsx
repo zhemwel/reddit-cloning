d0c715962aa317937147016cdfa1cf66df6177df
shemwel12 <shemwell@gagasimaji.com>
2022-06-16 14:42:45 +0700

    Fixing

--- components/SubredditRow.tsx
+++ components/SubredditRow.tsx
@@ -13,7 +13,7 @@ const SubredditRow = ({index, topic}: Props) => {
     <div className="flex items-center space-x-2 border-t bg-white px-4 py-2 last:rounded-b">
       <p>{index + 1}</p>
       <ChevronUpIcon className="h-4 w-4 flex-shrink-0 text-green-400" />
-      <Avatar seed={`/subreddit/${topic}`} />
+      <Avatar seed={`${topic}`} />
       <p className="flex-1 truncate">r/{topic}</p>
       <Link href={`/subreddit/${topic}`} >
         <div className="cursor-pointer rounded-full bg-blue-500 px-3 text-white">
