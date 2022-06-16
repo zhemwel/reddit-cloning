4db15c217dd0aa2267a22747666e5bbbf62b0778
shemwel12 <shemwell@gagasimaji.com>
2022-06-16 14:15:17 +0700

    bug fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { ApolloProvider } from '@apollo/client'
 import client from "../apollo-client";
 import { Toaster } from 'react-hot-toast'
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+function MyApp({ Component, pageProps: {session, ...pageProps} }: any) {
   return (
     <ApolloProvider client={client}>
       <SessionProvider session={session}>
